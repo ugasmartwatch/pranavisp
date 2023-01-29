3ff2c2d6b6ec0948622f716c1e417f4945a8eb2b
pranavisp <123051146+pranavisp@users.noreply.github.com>
2023-01-28 19:45:14 -0500

    Update watchFace.js

--- watchFace.js
+++ watchFace.js
@@ -20,12 +20,14 @@ function draw() {
   } else if (h == 0) {
     h = 12;
   }
+  g.setColor("#FDFDFD");  
   var time = (" "+h).substr(-2) + ":" + ("0"+m).substr(-2);
   // Reset the state of the graphics library
   g.reset();
   // draw the current time (4x size 7 segment)
   g.setFont("7x11Numeric7Seg",3.5);
   g.setFontAlign(1,2); // align right bottom
+  g.setColor("#FDFDFD");  
   g.drawString(time, X-35, Y-10, false);
 //  draw.setColor("#140005");
 
@@ -33,38 +35,41 @@ function draw() {
   g.setFont("6x8");
   g.setFontAlign(0,5); // align center bottom
   // pad the date - this clears the background if the date were to change length
+  
   var dateStr = "    "+require("locale").date(d)+"    ";
+  g.setColor("#FDFDFD");  
   g.drawString(dateStr, g.getWidth()/2, Y+15, false);
-  g.setColor("#140005");
   // AM/PM
   var ampm = "PM";
   if (am) {
     ampm = "AM";
   }
+
   g.drawString(ampm, 140, 120, false);
   // Custom
   g.setFontAlign(0,0).setFont("Vector",18);
   if (am) {
+    g.setColor("#FDFDFD");  
     g.drawString("Good morning,", g.getWidth()/2, 40, false);
     g.setBgColor("#FFA500");
-    g.setColor("#00FF00"); // orange
   } else if ((h >= 9) && (m >= 30)) {
+    g.setColor("#FDFDFD");  
     g.drawString("Good night,", g.getWidth()/2, 40, false);
-    g.setColor("#050A30");// dark blue
+    g.setBgColor("#050A30");// dark blue
   } else if (h >= 6) {
-     g.setColor("#0000FF"); // evening light blue color
-     g.fillRect(0, 26, 175, 175);
+    g.setBgColor("#0000FF"); // evening light blue color
+    g.setColor("#FDFDFD");  
     g.drawString("Good evening,", g.getWidth()/2, 40, false);
-    g.setColor(0, 0, 0);
-  
+
   } else {
+    g.setColor("#FDFDFD");  
     g.drawString("Good afternoon,", g.getWidth()/2, 40, false);
   }
   // Name
   g.setFontAlign(0,0).setFont("Vector",32);
-  
+  g.setColor("#FDFDFD");  
   g.drawString("Pranavi!", g.getWidth()/2, 70, false);
- g.setColor(0, 0, 0);
+
 
 }
 
